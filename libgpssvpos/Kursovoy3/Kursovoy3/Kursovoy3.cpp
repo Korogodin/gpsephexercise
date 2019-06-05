a0758c3c3463e1e48eb37f200dc7f1aad5c5f967
Andrew Opanasenko <35809906+andrew-opanasenko@users.noreply.github.com>
2019-06-06 00:06:16 +0300

    Курсовой проект. Часть 3. Исправление 1.
    Нашел способ как запустить тесты под виндой. Ошибок при сборке нет. Исключения не выбрасываются. Тесты не проходятся. Пробовал двумя методами: сравнивать каждую координату с координатой из матлаба и сравнение сумм координат.

---
+++ libgpssvpos/Kursovoy3/Kursovoy3/Kursovoy3.cpp
@@ -0,0 +1,151 @@
+#include "pch.h"
+#include <iostream>
+#include <map>
+#include <vector>
+#include "time.h"
+#include <string>
+//using namespace System;
+//using namespace System::Text;
+//using namespace System::Collections::Generic;
+using std::map;
+using std::vector;
+using std::string;
+
+#define pi 3,1415926535
+
+
+// Параметры, полученные со спутника
+map<string, float> parameters = { {"A", 26560971.874},
+								  {"M0", 2.01704},
+								  {"Toe", 288000},
+								  {"omegaZero", 55.82613},
+								  {"omega", 98.53938},
+								  {"omegaDot", -0.00000046497},
+								  {"omegaDotE", 0.000072921151467},
+								  {"eccentricity", 0.00146475},
+								  {"inclination", 54.57146},
+								  {"motionDiff", 0.00000027403},
+								  {"M", 398600500000000},
+								  {"IDOT", -0.000000018028},
+								  {"Cus", 0.0000086408},
+								  {"Cuc", -0.00000086054},
+								  {"Crs", -1.5281},
+								  {"Crc", 207.94},
+								  {"Cis", -0.000000042841},
+								  {"Cic", -0.000000087544} };
+
+// Функция, реализующая решение уравнения Кеплера
+float solveKeplersEquation(float meanAnomaly, float eccentricity, float accuracy) {
+
+	float prevEk = 0;
+	float keplersSolution = 0;
+
+	while (true) {
+		keplersSolution = meanAnomaly + eccentricity * sin(prevEk);
+
+		if (abs(prevEk - keplersSolution) <= accuracy) {
+			break;
+		}
+
+		prevEk = keplersSolution;
+	}
+
+	return keplersSolution;
+}
+
+// Перевод из градусов в радианы
+float degToRad(float degree) {
+	return degree * (pi / 180);
+}
+
+// Вычисление положения спутника на заданный момент времени
+map<string, vector<float>> findPositionOfSatellite(map<string, float>& parameters, int momentOfTime) {
+
+	map<string, vector<float>> coordinatesOfSatellite;
+	vector<float> coordinatesInEcefSystem(3);
+	vector<float> coordinatesInEciSystem(3);
+
+
+	parameters["Toe"]         = parameters["Toe"] + 18;
+	parameters["M0"]		  = degToRad(parameters["M0"]);
+	parameters["omegaZero"]   = degToRad(parameters["omegaZero"]);
+	parameters["omega"]       = degToRad(parameters["omega"]);
+	parameters["omegaDot"]    = degToRad(parameters["omegaDot"]);
+	parameters["inclination"] = degToRad(parameters["inclination"]);
+	parameters["motionDiff"]  = degToRad(parameters["motionDiff"]);
+	parameters["IDOT"]        = degToRad(parameters["IDOT"]);
+
+	int Tk = momentOfTime - parameters["Toe"];
+	// Set accuracy of calculations
+	float accuracy = pow(10, -8);
+
+	if (Tk > 302400) {
+		Tk = Tk - 604800;
+	}
+	else if (Tk < -302400) {
+		Tk = Tk + 604800;
+	}
+
+	// Compute mean motion
+	float n0 = pow(parameters["M"] / (pow(parameters["A"], 3)), 0.5);
+	// Correct mean motion
+	float n = n0 + parameters["motionDiff"];
+	// Mean anomaly
+	float meanAnomaly = parameters["M0"] + n * Tk;
+
+	// Solve Keplers equation
+	float Ek = solveKeplersEquation(meanAnomaly, parameters["eccentricity"], accuracy);
+	// Callulate a true anomaly
+	float Vk = atan2((pow((1 - parameters["eccentricity"] * parameters["eccentricity"]), 0.5) * sin(Ek) / (1 - parameters["eccentricity"] * cos(Ek))), ((cos(Ek) - parameters["eccentricity"]) / (1 - parameters["eccentricity"] * cos(Ek))));
+
+	// Argument of Latitude
+	float Fk = Vk + parameters["omega"];
+	// Second harmonic perturbations
+	float deltaUk = parameters["Cus"] * sin(2 * Fk) + parameters["Cus"] * cos(2 * Fk);
+	float deltaRk = parameters["Crs"] * sin(2 * Fk) + parameters["Crc"] * cos(2 * Fk);
+	float deltaIk = parameters["Cis"] * sin(2 * Fk) + parameters["Cic"] * cos(2 * Fk);
+	// Correct argument of Latitude
+	float Uk = Fk + deltaUk;
+	// Correct radius
+	float Rk = parameters["A"] * (1 - parameters["eccentricity"] * cos(Ek)) + deltaRk;
+	// Correct inclination
+	float Ik = parameters["inclination"] + deltaIk + parameters["IDOT"] * Tk;
+	// Correct longitude of ascending node
+	float Wk = parameters["omegaZero"] + (parameters["omegaDot"] - parameters["omegaDotE"]) * Tk - parameters["omegaDotE"] * parameters["Toe"];
+
+	// Positions in orbitalplane
+	float x = Rk * cos(Uk);
+	float y = Rk * sin(Uk);
+	// Earth-fixed coordinates
+	// Coordinates in ECEF system
+	float ecefX = x * cos(Wk) - y * cos(Ik) * sin(Wk);
+	float ecefY = x * sin(Wk) + y * cos(Uk) * cos(Wk);
+	float ecefZ = y * sin(Ik);
+
+	// Coordinates in ECI system
+	float theta = parameters["omegaDot"] * Tk;
+
+	float eciX = ecefX * cos(theta) - ecefY * sin(theta);
+	float eciY = ecefX * sin(theta) + ecefY * cos(theta);
+	float eciZ = ecefZ;
+
+	coordinatesInEcefSystem[0] = ecefX;
+	coordinatesInEcefSystem[1] = ecefY;
+	coordinatesInEcefSystem[2] = ecefZ;
+
+	coordinatesInEciSystem[0] = eciX;
+	coordinatesInEciSystem[1] = eciY;
+	coordinatesInEciSystem[2] = eciZ;
+
+	coordinatesOfSatellite["coordinatesInEcef"] = coordinatesInEcefSystem;
+	coordinatesOfSatellite["coordinatesInEci"] = coordinatesInEciSystem;
+
+	return coordinatesOfSatellite;
+}
+
+int main()
+{
+
+	//Console::WriteLine(L"Unit-test in MS Visual Studio.");
+    return 0;
+}
---
+++ libgpssvpos/UnitTest/TestGPS/TestGPS.cpp
@@ -0,0 +1,107 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "time.h"
+#include <iostream>
+#include <fstream>
+#include "C:\Users\andre\Desktop\MVS tests\Kursovoy3\Kursovoy3\Kursovoy3.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using std::ifstream;
+
+namespace TestGPS
+{
+	TEST_CLASS(TestGPS)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// Associative array for output data from C++ programm
+			map<string, vector<float>> coordinatesOfSatellite;
+			// Associative array for data from Matlab programm
+			vector<float> xCoordsFromMatlab;
+			vector<float> yCoordsFromMatlab;
+			vector<float> zCoordsFromMatlab;
+
+			//  Reading array of coordinates calculating in Matlab
+			ifstream file1("C:\\Users\\andre\\Desktop\\MVS tests\\x_coords.txt");
+			ifstream file2("C:\\Users\\andre\\Desktop\\MVS tests\\y_coords.txt");
+			ifstream file3("C:\\Users\\andre\\Desktop\\MVS tests\\z_coords.txt");
+
+			if (file1 && file2 && file3) {
+
+				vector<string> vecStr1, vecStr2, vecStr3;
+				string str1, str2, str3;
+
+				while (true) {
+					str1.clear();
+					str2.clear();
+					str3.clear();
+
+					getline(file1, str1);
+					getline(file2, str2);
+					getline(file3, str3);
+
+					if (!str1.empty() && !str2.empty() && !str3.empty()) {
+						vecStr1.push_back(str1);
+						vecStr2.push_back(str2);
+						vecStr3.push_back(str3);
+					} else {
+						break;
+					}
+				}
+
+				for (int i = 0; i < vecStr1.size(); ++i) {
+					for (int j = 0; j < vecStr1[i].size(); ++j) {
+						xCoordsFromMatlab.push_back(vecStr1[i][j]);
+					}
+				}
+
+				for (int i = 0; i < vecStr2.size(); ++i) {
+					for (int j = 0; j < vecStr2[i].size(); ++j) {
+						yCoordsFromMatlab.push_back(vecStr2[i][j]);
+					}
+				}
+
+				for (int i = 0; i < vecStr3.size(); ++i) {
+					for (int j = 0; j < vecStr3[i].size(); ++j) {
+						zCoordsFromMatlab.push_back(vecStr3[i][j]);
+					}
+				}
+
+				file1.close();
+				file2.close();
+				file3.close();
+			}
+
+			// Time of starting the programm
+			unsigned int startTime = clock();
+			float sum1 = 0;
+			float sum2 = 0;
+
+			for (int i = 0; i < 42199; i++) {
+
+				float momentOfTime = 30418 + i;
+
+				coordinatesOfSatellite = findPositionOfSatellite(parameters, momentOfTime);
+
+				//Assert::AreEqual(coordinatesOfSatellite["coordinatesInEci"][0], xCoordsFromMatlab[i]);
+				//Assert::AreEqual(coordinatesOfSatellite["coordinatesInEci"][1], yCoordsFromMatlab[i]);
+				//Assert::AreEqual(coordinatesOfSatellite["coordinatesInEci"][2], zCoordsFromMatlab[i]);
+				
+				sum1 = sum1 + (coordinatesOfSatellite["coordinatesInEci"][0] + coordinatesOfSatellite["coordinatesInEci"][1] + coordinatesOfSatellite["coordinatesInEci"][2]);
+				sum2 = sum2 + (xCoordsFromMatlab[i] + yCoordsFromMatlab[i] + zCoordsFromMatlab[i]);
+				
+			}
+
+			bool tmp = false;
+
+			if (abs(sum1 - sum2) < 100000) {
+				tmp = true;
+			}
+
+			Assert::AreEqual(tmp, true);
+
+			std::cout << (startTime / CLOCKS_PER_SEC);
+		}
+	};
+}
